15747024805a26b009f4db116a12ecaadfa7a8f2
ARSHHisPro <ctrlquest18@gmail.com>
2026-06-24 15:45:34 +0530

    Update server token and channel names in bot

--- aternos_server_bot.py
+++ aternos_server_bot.py
@@ -3,11 +3,11 @@ import os
 from python_aternos import Client
 import time
 
-TOKEN = 'paste-your-token-here'
+TOKEN = 'MTUxOTI4MDY0MjkyNjMxNzU5OA.Gp1YR4.dICNBqnreSIVt4EpN962tyfopSIzI4kLQ_nmJI'
 
 client = discord.Client()
 
-aternos = Client('your-aternos-username', password='your-aternos-password')
+aternos = Client('Betilabs1', password='123456')
 
 atservers = aternos.servers
 
@@ -28,24 +28,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.channel.name == 'bot-cmnds':
+    if message.channel.name == 'minecraft-smp':
         if user_message.lower() == '?hello':
             await message.channel.send(f'Hello {username}!')
             return
 
-    if message.channel.name == 'bot-cmnds':
+    if message.channel.name == 'minecraft-smp':
         if user_message.lower() == '?server_start':
           myserv.start()
           while True:
-            ping = str(os.popen('mcstatus yourservername.aternos.me status | grep description').read())
+            ping = str(os.popen('mcstatus DevuncopySMP.aternos.me status | grep description').read())
             if "offline" in ping:
               time.sleep(1)
             else:
               break
-          await message.channel.send("server is now alive!!! you can join in 2-3 minutes by pasting ||yourservername.aternos.me:serverport|| in the server address.")
+          await message.channel.send("server is now alive!!! you can join in 2-3 minutes by pasting ||DevuncopySMP.aternos.me:60456|| in the server address.")
           return
 
-    if message.channel.name == 'bot-cmnds':
+    if message.channel.name == 'minecraft-smp':
         if user_message.lower() == '?server_stop':
           myserv.stop()
           await message.channel.send(f'server stopped')
